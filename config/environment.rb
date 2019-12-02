3f8792c1d63d1a0654bd676628afd199cdea7cbc
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-02 16:02:01 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,26 @@ require 'pry'
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
+
+module InstanceMethods
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    self.all.count
+  end
+
+
+end
